174c7e0aafd5629f2cfcd8960e23e4410e8a3729
Pratham <22pkhanna@heckgrammar.co.uk>
2025-03-28 10:15:11 +0000

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,4 +1,4 @@
-use std::io;
+*/ use std::io;
 
 fn main() {
     // Create a new String to store user input
@@ -14,4 +14,22 @@ fn main() {
     
     // Trim and print the input
     println!("You entered: {}", input.trim());
+}*/
+
+use std::io;
+fn main() {
+       println!("Guess the number!");
+      let mut input = String::new();
+      println!("Please enter a 1 digit number:");
+      io::stdin()
+          .read_line(&mut input)
+          .expect("Failed to read line");
+      println!("You entered: {}", input.trim());
+      let number: i32 = 200;
+      if input.trim() == "7" {
+        println!("You win!");
+      }
+      else {
+        println!("You lose!") 
+      }  
 }
